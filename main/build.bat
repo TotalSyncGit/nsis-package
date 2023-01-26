f8c375ef773b70e4eb7e927162f76882628fcfa6
George Tatar <george@akruto.com>
2023-01-26 14:48:41 -0500

    Changed from FriendlySync to TotalSync

--- main/build.bat
+++ main/build.bat
@@ -1,3 +1,3 @@
 del *.nupkg
 nuget pack -Properties NoWarn=NU5100 nsis.nuspec
-nuget sign *.nupkg -CertificateSubjectName FriendlySync -Timestamper http://timestamp.digicert.com
+nuget sign *.nupkg -CertificateSubjectName TotalSync -Timestamper http://timestamp.digicert.com
