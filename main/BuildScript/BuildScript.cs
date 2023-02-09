40430aac86552114b6f7ae0e67186cdeaf107dad
George Tatar <george@akruto.com>
2023-02-09 17:57:07 -0500

    Use C# build script

---
+++ main/BuildScript/BuildScript.cs
@@ -0,0 +1,12 @@
+﻿// Hello World! program
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine("Current Directory: " + cwd);
+        }
+    }
+}
