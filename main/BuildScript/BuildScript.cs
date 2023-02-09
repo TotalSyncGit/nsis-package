4dc0730ee90598660e5a9098940767b3e4ebb875
George Tatar <george@akruto.com>
2023-02-09 18:00:35 -0500

    Print CodeBase

--- main/BuildScript/BuildScript.cs
+++ main/BuildScript/BuildScript.cs
@@ -1,12 +1,19 @@
 ﻿// Hello World! program
+using System.Reflection;
+
 namespace HelloWorld
 {
     class Hello
     {
         static void Main(string[] args)
         {
+            // Find directory paths
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+
             string cwd = Directory.GetCurrentDirectory();
+
             Console.WriteLine("Current Directory: " + cwd);
+            Console.WriteLine("CodeBase: " + codeBase);
         }
     }
 }
