badbbf8f8072931eb690768fa99962930fcad17b
George Tatar <george@akruto.com>
2023-12-05 13:02:56 -0500

    Changed Certificate Subject name to Akruto, Inc.

--- main/build.bat
+++ main/build.bat
@@ -1,3 +1,3 @@
 del *.nupkg
 nuget pack -Properties NoWarn=NU5100 nsis.nuspec
-nuget sign *.nupkg -CertificateSubjectName TotalSync -Timestamper http://timestamp.digicert.com
+nuget sign *.nupkg -CertificateSubjectName "Akruto, Inc" -Timestamper http://timestamp.digicert.com
